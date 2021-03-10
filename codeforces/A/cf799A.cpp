0dca755462a287ef9a72d9b3ff407d3452a74cd9
Aragami1408 <vucaominh1408@gmail.com>
2021-03-10 13:29:21 +0700

    Modified gitignore

---
+++ codeforces/A/cf799A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#define ll long long
+
+int main() {
+    ll n,t,k,d;
+    bool res;
+
+    std::cin >> n >> t >> k >> d;
+
+    if (k >= n) {
+        res = false;
+    }
+    else {
+
+    }
+
+
+    if (res == true) {
+        std::cout << "YES" << std::endl;
+    }
+    else {
+        std::cout << "NO" << std::endl;
+    }
+}
+
